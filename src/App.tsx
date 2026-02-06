703ae4e7a03798cde5e71a991f108df329ae941a
Amy Mendoza <amendoza@udel.edu>
2026-02-05 21:22:39 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript - Amy Mendoza
             </header>
+            
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
