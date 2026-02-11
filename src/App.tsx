a30a3af5c3a75fae0584060db2c5fa81260d4ac0
Amy Mendoza <amendoza@udel.edu>
2026-02-11 09:36:33 -0500

    Added 'Hello World'

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App(): React.JSX.Element {
                 automatically reload.
             </p>
             <p>
-                Hello World!
+                Hello World!!
             </p>
         </div>
     );
