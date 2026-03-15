f47504f6e6d0f0b3fb322e07d9006ce617fc84aa
Amy Mendoza <amendoza@udel.edu>
2026-03-15 18:25:14 -0400

    implemented GiveAttempt

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,37 @@
+
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [remainingattmpt, setRemainingattmpt] = useState<number>(3)
+    const [requestedattmpt, setRequestedattmpt] = useState<string>("")
+
+    function updateRequestedAttempts(event: React.ChangeEvent<HTMLInputElement>){
+        setRequestedattmpt(event.target.value)
+    }
+    function gainAttempt(){
+        let amount = parseInt(requestedattmpt)
+        if(!isNaN(amount)){
+            setRemainingattmpt(remainingattmpt + amount)
+        }
+    }
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <div>
+                Remaining Attempts: <span>{remainingattmpt}</span>
+            </div>
+            <Form.Group controlId = "attemptRequest">
+                <Form.Label>Request attempts</Form.Label>
+                <Form.Control  
+                    type ="number"
+                    value = {requestedattmpt}
+                    onChange ={updateRequestedAttempts}       
+                    />
+            </Form.Group>
+            
+                <Button onClick={()=>{setRemainingattmpt(remainingattmpt - 1)}} disabled = {remainingattmpt === 0}>Use</Button>
+                <Button onClick={()=>{gainAttempt()}}>gain</Button>
+
         </div>
     );
 }
