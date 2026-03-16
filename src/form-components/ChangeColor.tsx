3c46164d66565b64e64b1173c119dfba3da6ed24
Amy Mendoza <amendoza@udel.edu>
2026-03-15 20:36:59 -0400

    Implemented all remaining form components

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
+import { Form } from "react-bootstrap";
 
+export const COLORS = ["red", "blue", "green", "yellow", "purple", "orange", "cyan", "magenta"];
 export function ChangeColor(): React.JSX.Element {
+    const [color, setColor] = useState<string>("red")
+
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>){
+        setColor(event.target.value)
+    }
     return (
         <div>
-            <h3>Change Color</h3>
+            {COLORS.map((col: string) => 
+                <Form.Check 
+                inline
+                type = "radio"
+                name = "color"
+                id = {`color-check-${col}`}
+                label = {col}
+                value = {col} 
+                checked = {col === color}
+                onChange = {updateColor}
+                key={col}
+                accent-color = {col}
+             />
+             )}
+             <div> 
+                You have chosen <span data-testid="colored-box" style={{ backgroundColor: color }}>{color}</span>
+             </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
-export function MultipleChoiceQuestion({
-    options,
-    expectedAnswer,
-}: {
-    options: string[];
-    expectedAnswer: string;
-}): React.JSX.Element {
+export function MultipleChoiceQuestion({options, expectedAnswer,}: {options: string[]; expectedAnswer: string;}): React.JSX.Element {
+    const [currentChoice, setCurrentChoice] = useState<string>(options[0])
+
+     function updateCurrentChoice(event: React.ChangeEvent<HTMLSelectElement>){
+            setCurrentChoice(event.target.value)
+        }
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <Form.Group controlId = "multipleChoice"> 
+                <Form.Label>Choose an answer</Form.Label>
+                <Form.Select value = {currentChoice} onChange={updateCurrentChoice}>
+                    {options.map((option: string) => 
+                    <option value = {option} key = {option}>{option}</option>)}
+                </Form.Select>
+            </Form.Group>
+            <div>{currentChoice === expectedAnswer ?"✔️": "❌"}</div>
         </div>
     );
 }
